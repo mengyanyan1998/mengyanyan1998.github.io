7e614b4f491022f863945b97b03ad499102cb358
mengyanyan1998 <3137802078@qq.com>
2017-12-08 14:04:17 +0800

    动画

--- weichangjing/js/script.js
+++ weichangjing/js/script.js
@@ -10,4 +10,14 @@ var swiper = new Swiper('.swiper-container', {
     navigation: {
         nextEl: '.swiper-button-next',
     },
+    //Swiper4.x的动画效果开始
+    on:{
+        init: function(){
+            swiperAnimateCache(this); //隐藏动画元素
+            swiperAnimate(this); //初始化完成开始动画
+        },
+        slideChangeTransitionEnd: function(){
+            swiperAnimate(this); //每个slide切换结束时也运行当前slide动画
+        }
+    }
 });
