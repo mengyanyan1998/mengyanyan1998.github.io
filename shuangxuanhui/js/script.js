da446875528ab943ab0eddd6d33e893db2449846
mengyanyan1998 <3137802078@qq.com>
2017-12-15 15:46:03 +0800

    修改

--- shuangxuanhui/js/script.js
+++ shuangxuanhui/js/script.js
@@ -23,3 +23,21 @@ var swiper = new Swiper('.swiper-container', {
         }
     }
 });
+
+var mp3=document.querySelector("#mp3");
+
+//音乐按钮
+var mp3Btn=document.querySelector("#mp3Btn")
+//暂停音乐
+//mp3Btn.style.animationPlayState='paused';
+//旋转音乐按钮
+//mp3Btn.style.animationPlayState='running'
+mp3Btn.onclick=function(){
+    if(mp3.paused){
+        mp3Btn.style.animationPlayState='running';
+        mp3.play();
+    }else{
+        mp3Btn.style.animationPlayState='paused';
+        mp3.pause();
+    }
+}
